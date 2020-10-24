3e978890ebf49a4f439f1ddaa845b0a6fea27f4d
Oliver Rdz <oliver.rdz@gmail.com>
2020-10-24 12:26:22 -0500

    Fixed marker

--- plots.py
+++ plots.py
@@ -3,14 +3,14 @@
 import matplotlib.pyplot as plt
 
 def plot(x, y, xlab, ylab, mark="-"):
-    plt.plot(x, y)
+    plt.plot(x, y, mark)
     plt.xlabel(xlab, fontsize=18)
     plt.ylabel(ylab, fontsize=18)
     plot_format()
 
 def plot2(x1, y1, x2, y2, xlab, ylab, lab1, lab2, mark1="o", mark2="-"):
-    plt.plot(x1, y1, label=lab1, marker=mark)
-    plt.plot(x2, y2, label=lab2, marker=mark)
+    plt.plot(x1, y1, mark1, label=lab1)
+    plt.plot(x2, y2, mark2, label=lab2)
     plt.xlabel(xlab, fontsize=18)
     plt.ylabel(ylab, fontsize=18)
     plot_format()
