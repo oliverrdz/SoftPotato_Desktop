e01e3204097453ffab7c8343233b28dcd84f4e75
Oliver Rdz <oliver.rdz@gmail.com>
2020-10-24 12:27:13 -0500

    Removed FD class and replaced with Simulate

--- simulation.py
+++ simulation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import numpy as np
 from scipy.linalg import solve_banded
@@ -9,76 +9,39 @@ R = 8.315 # J/mol K, Gas constant
 T = 298 # K, Temperature
 FRT = F/(R*T)
 
-class FD:
+class Simulate:
 
-    def __init__(self, wf, n=1, Ageo=1, cOb=1e-6, cRb=1e-6, DO=1e-5, DR=1e-5, 
-                 E0=0, ks=1e8, alpha=0.5):
-        E = wf.E
-        t = wf.t
+    def __init__(self, wf, spc, mec, Ageo=1):
         
-        DOR = DO/DR
-
-        nT = np.size(t)
-        dT = 1/nT
-        lamb = 0.45
-        #%% Simulation parameters
-        nT = np.size(t) # number of time elements
-        dT = 1/nT # adimensional step time
-        lamb = 0.45 # For the algorithm to be stable, lamb = dT/dX^2 < 0.5
-        Xmax = 6*np.sqrt(nT*lamb) # Infinite distance
-        dX = np.sqrt(dT/lamb) # distance increment
-        nX = int(Xmax/dX) # number of distance elements
-
-        ## Discretisation of variables and initialisation
-        if cRb == 0: # In case only O present in solution
-            CR = np.zeros([nT,nX])
-            CO = np.ones([nT,nX])
-        else:
-            CR = np.ones([nT,nX])
-            CO = np.ones([nT,nX])*cOb/cRb
-
-
-        X = np.linspace(0,Xmax,nX) # Discretisation of distance
-        eps = (E-E0)*n*FRT # adimensional potential waveform
-        delta = np.sqrt(DR*t[-1]) # cm, diffusion layer thickness
-        K0 = ks*delta/DR # Normalised standard rate constant
+        eps = (wf.E-mec.E0)*mec.n*FRT # adimensional potential waveform
+        
+        #Emec = mec.E(wf, spc, DO, DR, cOb, cRb, ks, alpha, spc.lamb) # E mechanism
 
         #%% Simulation
-        for k in range(1,nT):
-            # Boundary condition, Butler-Volmer:
-            CR1kb = CR[k-1,1]
-            CO1kb = CO[k-1,1]
-            CR[k,0] = (CR1kb + dX*K0*np.exp(-alpha*eps[k])*(CO1kb + CR1kb/DOR))/(
-                       1 + dX*K0*(np.exp((1-alpha)*eps[k]) + np.exp(-alpha*eps[k])/DOR))
-            CO[k,0] = CO1kb + (CR1kb - CR[k,0])/DOR
-    
-            # Solving finite differences:
-            for j in range(1,nX-1):
-                CR[k,j] = CR[k-1,j] + lamb*(CR[k-1,j+1] - 2*CR[k-1,j] + CR[k-1,j-1])
-                CO[k,j] = CO[k-1,j] + DOR*lamb*(CO[k-1,j+1] - 2*CO[k-1,j] + CO[k-1,j-1])
+        for k in range(1,spc.nT):
+            mec.fd(k, mec.CR[k-1,1], mec.CO[k-1,1], eps[k])
 
         # Denormalising:
-        if cRb:
-            I = -CR[:,2] + 4*CR[:,1] - 3*CR[:,0]
-            D = DR
-            c = cRb
+        if mec.cRb:
+            I = -mec.CR[:,2] + 4*mec.CR[:,1] - 3*mec.CR[:,0]
+            D = mec.DR
+            c = mec.cRb
         else: # In case only O present in solution
-            I = CO[:,2] - 4*CO[:,1] + 3*CO[:,0]
-            D = DO
-            c = cOb
-        i = n*F*Ageo*D*c*I/(2*dX*delta)
+            I = mec.CO[:,2] - 4*mec.CO[:,1] + 3*mec.CO[:,0]
+            D = mec.DO
+            c = mec.cOb
+        i = mec.n*F*Ageo*D*c*I/(2*spc.dX*mec.delta)
 
-        cR = CR*cRb
-        cO = CO*cOb
-        x = X*delta
+        cR = mec.CR*mec.cRb
+        cO = mec.CO*mec.cOb
+        x = spc.X*mec.delta
 
-        self.E = E
-        self.t = t
+        self.E = wf.E
+        self.t = wf.t
         self.i = i
         self.cR = cR
         self.cO = cO
         self.x = x
-
 
 class BI:
 
@@ -154,3 +117,69 @@ class BI:
         self.cO = cO
         self.x = x
 
+class FD_before:
+
+    def __init__(self, wf, n=1, Ageo=1, cOb=1e-6, cRb=1e-6, DO=1e-5, DR=1e-5, 
+                 E0=0, ks=1e8, alpha=0.5):
+        E = wf.E
+        t = wf.t
+        
+        DOR = DO/DR
+
+        #%% Simulation parameters
+        nT = np.size(t) # number of time elements
+        dT = 1/nT # adimensional step time
+        lamb = 0.45 # For the algorithm to be stable, lamb = dT/dX^2 < 0.5
+        Xmax = 6*np.sqrt(nT*lamb) # Infinite distance
+        dX = np.sqrt(dT/lamb) # distance increment
+        nX = int(Xmax/dX) # number of distance elements
+
+        ## Discretisation of variables and initialisation
+        if cRb == 0: # In case only O present in solution
+            CR = np.zeros([nT,nX])
+            CO = np.ones([nT,nX])
+        else:
+            CR = np.ones([nT,nX])
+            CO = np.ones([nT,nX])*cOb/cRb
+
+
+        X = np.linspace(0,Xmax,nX) # Discretisation of distance
+        eps = (E-E0)*n*FRT # adimensional potential waveform
+        delta = np.sqrt(DR*t[-1]) # cm, diffusion layer thickness
+        K0 = ks*delta/DR # Normalised standard rate constant
+
+        #%% Simulation
+        for k in range(1,nT):
+            # Boundary condition, Butler-Volmer:
+            CR1kb = CR[k-1,1]
+            CO1kb = CO[k-1,1]
+            CR[k,0] = (CR1kb + dX*K0*np.exp(-alpha*eps[k])*(CO1kb + CR1kb/DOR))/(
+                       1 + dX*K0*(np.exp((1-alpha)*eps[k]) + np.exp(-alpha*eps[k])/DOR))
+            CO[k,0] = CO1kb + (CR1kb - CR[k,0])/DOR
+    
+            # Solving finite differences:
+            for j in range(1,nX-1):
+                CR[k,j] = CR[k-1,j] + lamb*(CR[k-1,j+1] - 2*CR[k-1,j] + CR[k-1,j-1])
+                CO[k,j] = CO[k-1,j] + DOR*lamb*(CO[k-1,j+1] - 2*CO[k-1,j] + CO[k-1,j-1])
+
+        # Denormalising:
+        if cRb:
+            I = -CR[:,2] + 4*CR[:,1] - 3*CR[:,0]
+            D = DR
+            c = cRb
+        else: # In case only O present in solution
+            I = CO[:,2] - 4*CO[:,1] + 3*CO[:,0]
+            D = DO
+            c = cOb
+        i = n*F*Ageo*D*c*I/(2*dX*delta)
+
+        cR = CR*cRb
+        cO = CO*cOb
+        x = X*delta
+
+        self.E = E
+        self.t = t
+        self.i = i
+        self.cR = cR
+        self.cO = cO
+        self.x = x
