694d87399786c9959e90c94773cb5e3e32783b3b
mamimela <64401253+mamimela@users.noreply.github.com>
2021-09-15 15:28:22 +0300

    Update fd
    Make use of numpy indexing to compute finite differences

--- sp.py
+++ sp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 ## Electrochemistry constants
-F = 96485 # C/mol, Faraday constant    
+F = 96485 # C/mol, Faraday constant
 R = 8.315 # J/mol K, Gas constant
 T = 298 # K, Temperature
 FRT = F/(R*T)
@@ -13,7 +13,7 @@ FRT = F/(R*T)
 ########## Potential Waveforms
 
 class Sweep:
-    """ 
+    """
     Returns t and E for a sweep potential waveform.
     All the parameters are given a default value.
     """
@@ -41,13 +41,13 @@ class Sweep:
 
 
 class Step:
-    """ 
+    """
     Returns t and E for a step potential waveform.
     All the parameters are given a default value.
     """
 
     def __init__(self, params):
-        
+
         self.Es = params[0]
         self.ttot = params[1]
         self.dt = params[2]
@@ -59,26 +59,26 @@ class Step:
 
 
 class Construct_wf:
-    """ 
-    
+    """
+
     Returns t and E for a customised potential waveform.
-    
+
     Parameters
     ----------
     wf:     list containing the waveform object
-    
+
     Returns
     -------
     t:      s, time array
     E:      V, potential array
-    
+
     Examples
     --------
     >>> import softpotato as sp
     >>> wf1 = sp.step(Estep, tini, ttot, dt)
     >>> wf2 = sp.sweep(Eini, Efin, sr, dE, ns)
     >>> wf = sp.Construct_wf([wf1, wf2])
-    
+
     Returns t and E calculated with the parameters given
     """
 
@@ -94,21 +94,21 @@ class Construct_wf:
         # Remove first data point to prevent repeating time
         self.t = t[1:]
         self.E = E[1:]
-        
+
 
 
 ########## Spacing:
 
 class Equal_spc:
-    """ 
-    
+    """
+
     Creates equal spacing in X and T
-    
+
     Parameters
     ----------
     wf:     waveform object
     lamb:   dT/dX^2 > 0.5 for stability (0.45)
-    
+
     Returns
     -------
     lamb:   dT/dX^2 > 0.5 for stability (0.45)
@@ -116,7 +116,7 @@ class Equal_spc:
     dX:     normalised distance increment, dX = np.sqrt(dT/lamb)
     nX:     number of distance elements, nX = Xmax/dX
     X:      normalised distance, X = to Xmax = 6*np.sqrt(nT*lamb)
-    
+
     Examples
     --------
     >>> import softpotato as sp
@@ -124,10 +124,10 @@ class Equal_spc:
     >>> wf2 = sp.sweep(Eini, Efin, sr, dE, ns)
     >>> wf = sp.Construct_wf([wf1, wf2])
     >>> space = Equal_spc(wf, lamb=0.45)
-    
+
     Returns t and E calculated with the parameters given
     """
-    
+
     def __init__(self, wf, lamb=0.45):
         t = wf.t
         self.lamb = lamb
@@ -139,7 +139,7 @@ class Equal_spc:
         dX = np.sqrt(dT/lamb) # distance increment
         nX = int(Xmax/dX) # number of distance elements
         X = np.linspace(0,Xmax,nX) # Discretisation of distance
-        
+
         self.lamb = lamb
         self.nT = nT
         self.nX = nX
@@ -152,7 +152,7 @@ class Equal_spc:
 class E_mec:
 
     def __init__(self, wf, space, params):
-        
+
         self.nT = space.nT
         self.nX = space.nX
         self.dX = space.dX
@@ -171,7 +171,7 @@ class E_mec:
         self.delta = np.sqrt(self.DR*wf.t[-1]) # cm, diffusion layer thickness
         self.K0 = self.ks*self.delta/self.DR # Normalised standard rate constant
         self.DOR = self.DO/self.DR
-        
+
         ## Discretisation of variables and initialisation
         if self.cRb == 0: # In case only O present in solution
             CR = np.zeros([self.nT,self.nX])
@@ -179,14 +179,14 @@ class E_mec:
         else:
             CR = np.ones([self.nT,self.nX])
             CO = np.ones([self.nT,self.nX])*self.cOb/self.cRb
-            
+
         self.CR = CR
         self.CO = CO
-        
-        
+
+
 
 ########## Simulation:
-        
+
 class Simulate:
 
     def __init__(self, wf, space, mec, Ageo=1):
@@ -199,7 +199,7 @@ class Simulate:
         self.CR = mec.CR
         self.BV = mec.BV
         print('Simulate ' + self.BV)
-        
+
     ## Set boundary conditions:
     def bc(self, CR1kb, CO1kb, eps):
         #if self.BV == 'QR': # Quasi reversible
@@ -211,7 +211,7 @@ class Simulate:
         #    CR = CR1kb/(1 + self.mec.dX*self.mec.K0*np.exp((1-self.mec.alpha)*eps))
         #    CO = CO1kb + (CR1kb - CR)/self.mec.DOR
         #elif self.BV == 'OR': # O -> R
-        #    CR = (CR1kb + self.mec.DOR*CO1kb)/(1 + np.exp(eps)) 
+        #    CR = (CR1kb + self.mec.DOR*CO1kb)/(1 + np.exp(eps))
         #    CO = CR*np.exp(eps)
 
         dX = self.mec.dX
@@ -233,26 +233,25 @@ class Simulate:
             CR = CR1kb + (CO1kb - CO)/DOR
 
         return CR, CO
-        
+
     def fd(self, progressBar=False): # Finite Differences
         for k in range(1,self.space.nT):
 
             if progressBar: # Active only when using GUI, updates progress bar
                 progressBar.setValue(int(100*k/self.space.nT))
 
-
             # Boundary condition, Butler-Volmer:
             self.CR[k,0], self.CO[k,0] = self.bc(self.CR[k-1,1], self.CO[k-1,1], self.eps[k])
-
-            # Solving finite differences:
-            for j in range(1,self.mec.nX-1):
-                self.CR[k,j] = self.CR[k-1,j] + self.mec.lamb*(self.CR[k-1,j+1] - 2*self.CR[k-1,j] + self.CR[k-1,j-1])
-                self.CO[k,j] = self.CO[k-1,j] + self.mec.DOR*self.mec.lamb*(self.CO[k-1,j+1] - 2*self.CO[k-1,j] + self.CO[k-1,j-1])
+            # Apply finite-differenc
+            self.CR[k,1:-1] = self.CR[k-1,1:-1] + self.mec.lamb*(self.CR[k-1,2:]\
+                            - 2*self.CR[k-1,1:-1] + self.CR[k-1,:-2])
+            self.CO[k,1:-1] = self.CO[k-1,1:-1] + self.mec.lamb*(self.CO[k-1, 2:]\
+                            - 2*self.CO[k-1, 1:-1] + self.CO[k-1,:-2])
 
             self.denorm()
 
     def denorm(self): # Denormalisation, I believe this can be optimised
-        
+
         if self.mec.cRb:
             I = -self.CR[:,2] + 4*self.CR[:,1] - 3*self.CR[:,0]
             D = self.mec.DR
@@ -277,10 +276,11 @@ class Simulate:
         self.cR = cR
         self.cO = cO
         self.x = x
-            
+
 ########## Plots:
-        
+
 class Plot_all:
+
     def __init__(self, sim):
         plt.subplot(221)
         Plot(sim.t, sim.E, "$t$ / s", "$E$ / V")
@@ -291,8 +291,8 @@ class Plot_all:
         plt.subplot(224)
         Plot2(sim.x, sim.cR[-1,:], sim.x, sim.cO[-1,:], "$x$ / cm", "$c$ / mol cm$^{-3}$", "$c_R$", "$c_O$")
         plt.show()
-        
-    
+
+
 class Plot:
     def __init__(self, x, y, xlab, ylab, mark="-"):
         plt.plot(x, y, mark)
